8c260af97b339c0e3f9c19506effe08db5e07329
sunil-saini <sunilsaini314@gmail.com>
2019-07-19 19:07:08 +0530

    script to run program and set cron

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from crontab import CronTab
-from main import get_current_user, get_current_directory, run
+import logging.config
+from common import get_current_user, get_current_directory, run, start_logging
+
+logger = logging.getLogger(__name__)
 
 
 def set_cron():
+    start_logging()
     username = get_current_user()
     cron = CronTab(user=username)
     command = "/bin/bash " + get_current_directory() + "/cron.sh " + get_current_directory()
     job = cron.new(command=command)
     job.minute.every(60)
     cron.write()
+    logger.info("cron successfully written for user: %s" % username)
 
 
 run()
