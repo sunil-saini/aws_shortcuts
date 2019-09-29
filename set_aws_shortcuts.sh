d9b5fd2ec026598988a76575271c75daa842613e
sunil-saini <sunilsaini314@gmail.com>
2019-09-29 12:45:21 +0530

    project setup and cron made consistent

--- set_aws_shortcuts.sh
+++ set_aws_shortcuts.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Started setting the project..."
 project="aws_shortcuts"
+echo "Detected OS - $OSTYPE"
+if ! [[ "$OSTYPE" == darwin* || "$OSTYPE" == "linux-gnu" ]]; then
+    echo "Unsupported OS, exiting"
+    exit 1
+fi
+
+echo "Started setting the project..."
 
 mkdir -p "$HOME/.$project"
-
 mkdir -p "$HOME/.$project/logs"
-
 cd "$HOME/.$project"
 
-git clone --quiet https://github.com/sunil-saini/aws_shortcuts.git >/dev/null
+if [[ -d "$project" ]]; then
+    cd "$project"
+    git pull --quiet origin master >/dev/null
+else
+    git clone --quiet https://github.com/sunil-saini/"$project".git >/dev/null
+    cd "$project"
+fi
 
-cd "$project"
 cron="$HOME/.$project/$project/cron.sh"
 
 chmod +x "$cron"
@@ -20,48 +29,17 @@ python -m pip install --ignore-installed -q -r requirements.txt --user
 
 echo "Started collecting data from AWS, it may take few minutes..."
 python driver.py
-echo "Collected data successfully, open new terminal tab to run the shortcut commands"
+echo "Data collected successfully"
 
-echo "Detected OS - $OSTYPE, setting cron.."
-if [[ $OSTYPE == darwin* ]]; then
-    label="com.$project"
-    plist="$label.plist"
-    plist_path="$HOME/Library/LaunchAgents/$plist"
-
-    cat << EOF > "$plist_path"
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>$label</string>
-
-      <key>ProgramArguments</key>
-      <array>
-        <string>$cron</string>
-      </array>
-
-      <key>Nice</key>
-      <integer>1</integer>
-
-      <key>StartInterval</key>
-      <integer>7200</integer>
-
-      <key>RunAtLoad</key>
-      <true/>
-
-    </dict>
-    </plist>
-EOF
-    launchctl load "$plist_path"
-
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-
-    crontab -l > current_cron
+crontab -l > current_cron
+cron_line="0 */2 * * * /bin/bash $cron"
+if grep -Fxq "$cron_line" current_cron; then
+    echo "Cron already set, skipping"
+else
     echo "0 */2 * * * /bin/bash $cron" >> current_cron
     crontab current_cron
     rm current_cron
-
-else
-    echo "Unsupported OS"
+    echo "Cron set successfully to keep updating data from AWS periodically"
 fi
+
+echo "Project set successfully, open new terminal tab and enjoy the shortcut commands"
