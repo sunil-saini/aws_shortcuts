af777afcd29e498b86c11e5e93e152b114121855
sunil-saini <sunilsaini314@gmail.com>
2019-10-08 04:58:15 +0530

    import added

--- cron.sh
+++ cron.sh
@@ -6,4 +6,4 @@ cd "$HOME/.$project/$project"
 
 echo "Updating local data, it may take few minutes..."
 
-python -c "from driver update_services_data as usd; usd()"
+python -c "from driver import update_services_data as usd; usd()"
