b14aba0ea980e39178d40c39cd0bc14d5caa6381
sunil-saini <sunilsaini314@gmail.com>
2019-10-15 13:55:18 +0530

    run script curl path updated

--- kickstart.sh
+++ kickstart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/sunil-saini/aws_shortcuts/bug%232/set_aws_shortcuts.sh -o set_aws_shortcuts.sh
+curl -s https://raw.githubusercontent.com/sunil-saini/aws_shortcuts/master/scripts/set_aws_shortcuts.sh -o set_aws_shortcuts.sh
 
 . ./set_aws_shortcuts.sh
