b3a3449122de266e4c2f5b1f17c3f02a49aed5d8
sunil-saini <sunilsaini314@gmail.com>
2019-10-15 15:00:06 +0530

    kickstart script added

--- kickstart.sh
+++ kickstart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/sunil-saini/aws_shortcuts/master/scripts/set_aws_shortcuts.sh -o set_aws_shortcuts.sh
+curl -s https://raw.githubusercontent.com/sunil-saini/aws_shortcuts/bug%232/scripts/set_aws_shortcuts.sh -o set_awss.sh
 
-. ./set_aws_shortcuts.sh
+. ./set_awss.sh
+
+rm set_awss.sh
